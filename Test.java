55402e4f99fb46e482e04eebabcea06168847145
wanyichao <923914186@qq.com>
2016-04-20 22:35:53 +0800

    '初始化提交'

---
+++ Test.java
@@ -0,0 +1,21 @@
+package com.briup.ch04;
+
+public class Test {
+	public static void main(String[] args){
+		test3();
+  }
+public static void test3(){
+	char c1='A';
+	char c2='жа';
+	char c3=65;
+	char c4=0x41;
+	char c5=0101;
+	char c6=0b1000001;
+	System.out.println(c1);
+	System.out.println(c2);
+	System.out.println(c3);
+	System.out.println(c4);
+	System.out.println(c5);
+	System.out.println(c6);
+}
+}
